f5c27d00d4b3e64802a98ab15b62f646f68d209d
Fady Damak <32818953+FadyDMK@users.noreply.github.com>
2024-11-30 21:23:51 +0100

    added file upload to local storage wih databse integration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 const path = require("path");
 const session = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
@@ -14,6 +14,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const helmet = require("helmet");
+const uploadMulter = multer({ dest: "uploads/" });
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -21,24 +23,24 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// app.use(
-//   session({
-//     cookie: {
-//       maxAge: 7 * 24 * 60 * 60 * 1000, // ms
-//     },
-//     secret: "a santa at nasa",
-//     resave: true,
-//     saveUninitialized: true,
-//     store: new PrismaSessionStore(new PrismaClient(), {
-//       checkPeriod: 2 * 60 * 1000, //ms
-//       dbRecordIdIsSessionId: true,
-//       dbRecordIdFunction: undefined,
-//     }),
-//   })
-// );
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
@@ -86,10 +88,30 @@ passport.deserializeUser(async (id, done) => {
 });
 
 app.get("/", (req, res) => {
-  console.log(req.user);
   res.render("index", { user: req.user });
 });
 
+app.get("/files", async (req, res) => {
+  const userFiles = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      Post: true, 
+    },
+  });
+  console.log(userFiles.Post);
+  res.render("files", { files: userFiles.Post || [] , user: req.user});
+});
+app.get("/download/:id", async (req, res) => {
+  const file = await prisma.file.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  res.download(file.path);
+  res.redirect("/files");
+})
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.get("/log-in", (req, res) => res.render("log-in"));
 app.get("/log-out", (req, res, next) => {
@@ -121,10 +143,26 @@ app.post("/sign-up", async (req, res, next) => {
 app.post(
   "/log-in",
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/files",
     failureRedirect: "/",
   })
 );
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const file = req.file;
+  const user = req.user;
+  if (!file) {
+    return res.status(400).send("No file uploaded");
+  }
+  await prisma.file.create({
+    data: {
+      name: file.originalname,
+      path: file.path, 
+      uploaderId: user.id, 
+    },
+  });
+  res.redirect("/files");
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("404 Not Found");
